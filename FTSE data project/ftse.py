fa384cb76f3a648b9fad9a0a313bb3d85debf47c
Adam <adambowieday3@gmail.com>
2024-10-11 19:20:31 +0100

    ADD changes to python test file

--- FTSE data project/ftse.py
+++ FTSE data project/ftse.py
@@ -1,1 +1,7 @@
-print("Hello world")
+import pandas as pd
+
+df = pd.read_csv("/Users/adam/repos/portfolio/FTSE data project/data/ftsetimes.csv")
+#print(df.to_string())
+
+df.fillna(0, inplace=True) #removing empty cells
+print(df.to_string())
